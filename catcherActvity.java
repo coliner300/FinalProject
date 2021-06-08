a3616d87e0a9c6c098df7ed8fe4313271b23f806
coliner300 <54290215+coliner300@users.noreply.github.com>
2021-06-08 12:36:12 -0700

    Update catcherActvity.java
    Added sound on back button for C

--- catcherActvity.java
+++ catcherActvity.java
@@ -1,6 +1,7 @@
 package com.example.finalproject;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,12 +19,14 @@ public class catcherActvity extends AppCompatActivity {
         Button backC;
 
         backC = findViewById(R.id.backC);
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.batsound);
 
         backC.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(catcherActvity.this, positions_activity.class));
+                mediaPlayer.start();
             }
         });
 
