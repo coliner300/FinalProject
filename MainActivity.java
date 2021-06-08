50ce6df28a75ccd14e104ffce2950d570f982fa9
coliner300 <54290215+coliner300@users.noreply.github.com>
2021-06-08 11:32:28 -0700

    Update MainActivity.java
    Updated main activity

--- MainActivity.java
+++ MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn1;
+    Button btn1, btn2, btn3;
 
 
     @Override
@@ -18,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btn1 = findViewById(R.id.positionButton);
+        btn2 = findViewById(R.id.playsButton);
+        btn3 = findViewById(R.id.levelsButton);
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.batsound);
 
         btn1.setOnClickListener(new View.OnClickListener()
         {
@@ -26,6 +29,28 @@ public class MainActivity extends AppCompatActivity {
             {
                 Intent intent = new Intent(MainActivity.this, positions_activity.class);
                 startActivity(intent);
+                mediaPlayer.start();
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(MainActivity.this, plays_activity.class);
+                startActivity(intent);
+                mediaPlayer.start();
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(MainActivity.this, differentLevels_activity.class);
+                startActivity(intent);
+                mediaPlayer.start();
             }
         });
     }
