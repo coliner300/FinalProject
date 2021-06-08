92d0eb353120444bd8428ca65abfb671e9c2a291
coliner300 <54290215+coliner300@users.noreply.github.com>
2021-06-08 12:33:22 -0700

    Update rightfield_activity.java
    Added sound on back button

--- rightfield_activity.java
+++ rightfield_activity.java
@@ -1,6 +1,7 @@
 package com.example.finalproject;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,12 +19,14 @@ public class rightfield_activity extends AppCompatActivity {
         Button backRF;
 
         backRF = findViewById(R.id.backRF);
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.batsound);
 
         backRF.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(rightfield_activity.this, positions_activity.class));
+                mediaPlayer.start();
             }
         });
 
