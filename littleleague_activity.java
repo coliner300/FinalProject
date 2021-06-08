d1f50fca886cd8eda43ffd930345ce9cbddad7d5
coliner300 <54290215+coliner300@users.noreply.github.com>
2021-06-08 11:09:52 -0700

    Create littleleague_activity.java
    Added little league activity

---
+++ littleleague_activity.java
@@ -0,0 +1,36 @@
+package com.example.finalproject;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class littleleague_activity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pitcher);
+
+        Button backLL;
+
+        backLL = findViewById(R.id.backLL);
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.batsound);
+
+        backLL.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(littleleague_activity.this, differentLevels_activity.class));
+                mediaPlayer.start();
+            }
+        });
+
+    }
+
+}
+
