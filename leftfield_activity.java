1c745f2ee105271467b6f00bec01a93e5055f274
coliner300 <54290215+coliner300@users.noreply.github.com>
2021-06-08 12:34:13 -0700

    Update leftfield_activity.java
    Added sound on back button for LF

--- leftfield_activity.java
+++ leftfield_activity.java
@@ -1,6 +1,7 @@
 package com.example.finalproject;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,12 +19,14 @@ public class leftfield_activity extends AppCompatActivity {
         Button backLF;
 
         backLF = findViewById(R.id.backLF);
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.batsound);
 
         backLF.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(leftfield_activity.this, positions_activity.class));
+                mediaPlayer.start();
             }
         });
 
