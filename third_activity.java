21ca35b6900f2d01a94764424f060ff19d03ff69
coliner300 <54290215+coliner300@users.noreply.github.com>
2021-06-08 12:34:42 -0700

    Update third_activity.java
    Added sound on back button for 3B

--- third_activity.java
+++ third_activity.java
@@ -1,6 +1,7 @@
 package com.example.finalproject;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,12 +19,14 @@ public class third_activity extends AppCompatActivity {
         Button backT;
 
         backT = findViewById(R.id.backT);
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.batsound);
 
         backT.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(third_activity.this, positions_activity.class));
+                mediaPlayer.start();
             }
         });
 
