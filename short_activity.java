d6efe7fc518e1b204bb73d40673f8ae6505cb81b
coliner300 <54290215+coliner300@users.noreply.github.com>
2021-06-08 12:35:07 -0700

    Update short_activity.java
    Added sound on back button for SS

--- short_activity.java
+++ short_activity.java
@@ -1,6 +1,7 @@
 package com.example.finalproject;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,12 +19,14 @@ public class short_activity extends AppCompatActivity {
         Button backSS;
 
         backSS = findViewById(R.id.backSS);
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.batsound);
 
         backSS.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(short_activity.this, positions_activity.class));
+                mediaPlayer.start();
             }
         });
 
